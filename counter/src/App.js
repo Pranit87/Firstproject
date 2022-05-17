eac985dbb49677dd1bcc15608ac84b3becdbd590
AbhishekDatar <abhirondatar@gmail.com>
2022-05-17 13:23:50 -0700

    Changes to router

--- counter/src/App.js
+++ counter/src/App.js
@@ -4,30 +4,22 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 import Counter from './components/counter';
-import Counters from "./components/counters";
-import Home from "./components/home";
-import About from "./components/about";
-import Contactus from "./components/contactus";
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import Counters from './components/counters';
+import Home from './components/home';
+import About from './components/about';
+import Contactus from './components/contactus';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-  <React.StrictMode>
-    <Router>
-      <Link to = ""> Home </Link>
-      <Link to = "/about"> About </Link>
-      <Link to = "/contactus"> Contact Us </Link>
-      <Route exact path = "/home" component = {Home} />
-      <Route path = "/about" component = {About} />
-      <Route path = "/contactus" component = {Contactus} />
-
-     
-    </Router>
-  </React.StrictMode>
-);
-
+    <>
+      <Link to=""> Home </Link>
+      <Link to="/about"> About </Link>
+      <Link to="/contactus"> Contact Us </Link>
+    </>
+  );
 }
 
 export default App;
--- counter/src/index.js
+++ counter/src/index.js
@@ -3,25 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 import Counter from './components/counter';
-import Counters from "./components/counters"
-import About from "./components/about"
-import Home from "./components/home"
-import Contactus from "./components/contactus"
-import {BrowserRouter as Router, Route,Routes, Link } from 'react-router-dom';
-
-
-
+import Counters from './components/counters';
+import About from './components/about';
+import Home from './components/home';
+import Contactus from './components/contactus';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div>
-    <App/>
-  </div>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+      <Route path="/about" element={<About />}></Route>
+      <Route path="/contactus" element={<Contactus />}></Route>
+    </Routes>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
