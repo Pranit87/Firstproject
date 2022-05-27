23d39374be09d7f493bb84347f0fff682fec2451
Pranit <hindalekar956@gmail.com>
2022-05-27 22:47:24 +0530

    new bootstrap

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
-import { useState } from "react";
-import {nanoid} from 'nanoid';
-import Noteslist from "./Components/NotesList";
+import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavbarComp from './Components/NavbarComp';
 
-const App = () => {
-    const [notes,setnotes] = useState([
-      {
-      id : nanoid(),
-      text: "This is my first note",
-      date: "13/08/2020",
-
-    },
-    {
-      id : nanoid(),
-      text: "This is my second note",
-      date: "14/08/2020",
-
-    },
-    {
-      id : nanoid(),
-      text: "This is my third note",
-      date: "15/08/2020",
-
-    },
-    {
-      id : nanoid(),
-      text: "This is my fourth note",
-      date: "16/08/2020",
-
-    },
-  ]);
-
-  return(
-    <div className = 'container'>
-      <Noteslist notes = {notes} />
+function App() {
+  return (
+    <div>
+      <NavbarComp/>
     </div>
   );
 }
